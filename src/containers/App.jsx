85be86d53fea7f29704bea87292da7fcb866e94f
Sebastian G. Q <sgq995@gmail.com>
2021-01-14 11:51:23 -0500

    feat: Added header

---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+import '../assets/styles/App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
+}
+
+export default App;
