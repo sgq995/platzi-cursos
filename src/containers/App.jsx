300f7762258aed570f9a0a4f0bc5b194293eba6e
Sebastian G. Q <sgq995@gmail.com>
2021-01-14 12:09:10 -0500

    feat: Added footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,25 +5,28 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Search />
+  return (
+    <div className="App">
+      <Header />
+      <Search />
 
-            <Categories>
-                <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
-        </div>
-    );
+      <Categories>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
