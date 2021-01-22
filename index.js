524c9d06e8deca5da1b4d5e007f48c2e80123564
Sebastian G. Q <sgq995@gmail.com>
2021-01-22 13:01:02 -0500

    chore: Nodemon added
    feat: Modified project directory for graphql

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 'use strict';
 
-const { graphql, buildSchema } = require('graphql');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const { buildSchema } = require('graphql');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+
+const resolvers = require('./lib/resolvers');
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-    greet: String
-  }
-`);
-
-const resolvers = {
-  hello: () => {
-    return 'Hello World!';
-  },
-
-  greet: () => {
-    return 'Hi!';
-  }
-}
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+);
 
 app.use('/api', graphqlHTTP({
   schema,
