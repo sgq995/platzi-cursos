1ca7e9c0baad92546d504c83195ff2decfb7ec25
Sebastian G. Q <sgq995@gmail.com>
2021-01-22 12:38:02 -0500

    feat(resolver): Added resolver

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const { graphql, buildSchema } = require('graphql');
 const schema = buildSchema(`
   type Query {
     hello: String
+    greet: String
   }
 `);
 
-graphql(schema, '{ hello }')
+const resolvers = {
+  hello: () => {
+    return 'Hello World!';
+  },
+
+  greet: () => {
+    return 'Hi!';
+  }
+}
+
+graphql(schema, '{ hello, greet }', resolvers)
   .then(data => console.log(data));
