3eddb4cbb0ad26199d8a139fc954f1cfc7f2916a
Sebastian G. Q <sgq995@gmail.com>
2021-01-22 12:34:59 -0500

    feat(schema): Initial schema

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { graphql, buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+graphql(schema, '{ hello }')
+  .then(data => console.log(data));
