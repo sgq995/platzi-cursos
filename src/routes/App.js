bec1d3b8eafc88ae2920eddf94d2bb0a70f7b27c
Sebastian G. Q <sgq995@gmail.com>
2021-01-28 14:32:02 -0500

    feat: Added login and register

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../containers/Home';
+import Login from '../containers/Login';
+import Register from '../containers/Register';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route
-        exact
-        path="/"
-        component={Home}
-      />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={Home}
+        />
 
-      
+        <Route
+          exact
+          path="/login"
+          component={Login}
+        />
+
+        <Route
+          exact
+          path="/register"
+          component={Register}
+        />
+      </Switch>
     </BrowserRouter>
   );
 };
