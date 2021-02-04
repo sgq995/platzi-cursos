7f2f7c135d4f4d8220ea9902b2ca38141ef7ac8d
Sebastian G. Q <sgq995@gmail.com>
2021-02-04 10:31:28 -0500

    feat(native): C++ module created

---
+++ native/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/addon/hello.o := g++ -o Release/obj.target/addon/hello.o ../hello.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DBUILDING_NODE_EXTENSION' -I/home/sebastian/.cache/node-gyp/15.8.0/include/node -I/home/sebastian/.cache/node-gyp/15.8.0/src -I/home/sebastian/.cache/node-gyp/15.8.0/deps/openssl/config -I/home/sebastian/.cache/node-gyp/15.8.0/deps/openssl/openssl/include -I/home/sebastian/.cache/node-gyp/15.8.0/deps/uv/include -I/home/sebastian/.cache/node-gyp/15.8.0/deps/zlib -I/home/sebastian/.cache/node-gyp/15.8.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c
+Release/obj.target/addon/hello.o: ../hello.cc \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/node.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/v8.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/cppgc/common.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/v8config.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/v8-internal.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/v8-version.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/v8config.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/v8-platform.h \
+ /home/sebastian/.cache/node-gyp/15.8.0/include/node/node_version.h
+../hello.cc:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/node.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/v8.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/cppgc/common.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/v8config.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/v8-internal.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/v8-version.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/v8config.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/v8-platform.h:
+/home/sebastian/.cache/node-gyp/15.8.0/include/node/node_version.h:
