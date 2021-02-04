164c44366aa05cf135c5adfc1d307ce0b63e98ed
Sebastian G. Q <sgq995@gmail.com>
2021-02-04 11:02:53 -0500

    feat(process): Process information and events

--- process.js
+++ process.js
@@ -1,32 +1,18 @@
-const { exec, spawn } = require('child_process');
-const { promisify } = require('util');
+// const process = require('process');
 
-exec('ls -la', (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    return false;
-  }
-
-  console.log(stdout);
+process.on('beforeExit', () => {
+  console.log('El proceso va a terminar');
 });
 
-(async function () {
-  const execPromise = promisify(exec);
-  try {
-    const { stdout, stderr } = await execPromise('ls -la');
-    console.log(stdout);
-  } catch (err) {
-    console.error(err);
-  }
-})();
-
-let process = spawn('ls', ['-la']);
-// console.log(process);
-
-process.stdout.on('data', function (data) {
-  console.log(data.toString());
+process.on('exit', () => {
+  console.log('El proceso acabo');
 });
 
-process.on('exit', function () {
-  console.log('The process has been completed');
+process.on('uncaughtException', (err, origin) => {
+  console.error(`Error form ${origin}`);
+  console.error(err);
 });
+
+thisDoesntExsists();
+
+console.log(`This doesn't show`)
