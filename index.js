1f9054d2ea0b03da8d0371bda2cdd47d38df6a40
Sebastian G. Q <sgq995@gmail.com>
2021-02-17 15:01:21 -0500

    feat(middlewares): Added middlewares

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const { config } = require('./config');
 
 const moviesApi = require('./routes/movies');
 
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/error-handler');
+const notFoundHandler = require('./utils/middleware/not-found-handler');
+
 // Body parser
 app.use(express.json());
 
+// Routes
 moviesApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Error middlewares
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening at http://localhost:${config.port}`);
