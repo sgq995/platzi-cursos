877f5c7454ec6a1ee0a1648bfd9064561c219278
Sebastian G. Q <sgq995@gmail.com>
2021-04-21 12:20:48 -0500

    feat(logo): Logo was added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCard'
+import { Logo } from './components/Logo'
 import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
