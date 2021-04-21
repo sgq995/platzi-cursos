5cf6e8723633c4c26d54169e70e5005199ba2d4d
Sebastian G. Q <sgq995@gmail.com>
2021-04-21 12:11:05 -0500

    chore: React icons added
    feat(photo_card): Photo card and list were added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCard'
 import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
   <>
     <GlobalStyles />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
