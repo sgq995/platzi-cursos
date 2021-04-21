ef7306f244db4691fb7726888bed0c88bd0a17f1
Sebastian G. Q <sgq995@gmail.com>
2021-04-21 14:00:02 -0500

    chore: Polyfill added
    feat(photo_card): Intersection observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -9,17 +9,24 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const ref = useRef(null)
   const [show, setShow] = useState(false)
 
-  useEffect(() => {
-    const observer = new window.IntersectionObserver(function (entries) {
-      const isIntersecting = entries[0].isIntersecting
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect()
-      } else {
-        setShow(false)
-      }
-    })
-    observer.observe(ref.current)
+  useEffect(function () {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    )
+      .then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const isIntersecting = entries[0].isIntersecting
+          if (isIntersecting) {
+            setShow(true)
+            observer.disconnect()
+          } else {
+            setShow(false)
+          }
+        })
+        observer.observe(ref.current)
+      })
   }, [ref])
 
   return (
