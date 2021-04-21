d68d9ba186642ba41e16c5cb975667212c0a0026
Sebastian G. Q <sgq995@gmail.com>
2021-04-21 10:21:03 -0500

    chore: Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js'
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
