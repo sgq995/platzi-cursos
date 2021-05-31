d24260ed3eddec4d1d0220d4061848eba0a987d3
Sebastian G. Q <sgq995@gmail.com>
2021-05-31 10:34:04 -0500

    feat: Own template engine

--- example/index.js
+++ example/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const expressJsx = require('./express-jsx');
+
+app.engine('jsx', expressJsx);
+
+app.set('views', './views');
+app.set('view engine', 'jsx');
 
 app.get('/', function (req, res, next) {
-  res.send('Hello world');
+  // res.send('Hello world');
+  res.render('index', {
+    hello: 'Hola',
+    world: 'mundo'
+  })
 });
 
 const server = app.listen(8000, function () { 
