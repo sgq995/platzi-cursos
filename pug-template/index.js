a104de1dbb6c07b781931bd718a4bdbc5c0f6d45
Sebastian G. Q <sgq995@gmail.com>
2021-05-31 10:45:31 -0500

    feat: Pug template engine

---
+++ pug-template/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const productsRouter = require('./routes/products');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/products', productsRouter);
+
+app.get('/', function (req, res, next) {
+  res.send({ hello: 'world' });
+});
+
+const server = app.listen(8000, function() {
+  console.log(server.address());
+  console.log(`Listening http://localhost:${server.address().port}`)
+});
