764d98012109d079b712b48755ba55b62e6b1b54
Sebastian G. Q <sgq995@gmail.com>
2021-10-20 14:56:57 -0500

    Pointers

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
-import (
-	"fmt"
-	mypkg "mypackage/mypackage"
-)
+import "fmt"
+
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc) ping() {
+	println(myPc.brand, "Pong")
+}
+
+func (myPc *pc) duplicateRAM() {
+	myPc.ram = 2 * myPc.ram
+}
+
+func (myPc pc) String() string {
+	return fmt.Sprintf("%d GB RAM, %d GB Disk (%s)", myPc.ram, myPc.disk, myPc.brand)
+}
 
 func main() {
-	var myCar mypkg.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2017
+	a := 50
+	b := &a
 
-	fmt.Println(myCar)
+	fmt.Println(a, b)
+
+	myPc := pc{ram: 16, disk: 200, brand: "msi"}
+	fmt.Println(myPc)
+
+	myPc.ping()
+	fmt.Println(myPc)
+
+	myPc.duplicateRAM()
+	fmt.Println(myPc)
 }
