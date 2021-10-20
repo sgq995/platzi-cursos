4507d3da07cbc24bef022f3c971204865d5ddc9a
Sebastian G. Q <sgq995@gmail.com>
2021-10-20 11:55:17 -0500

    Funciones

--- src/main.go
+++ src/main.go
@@ -2,21 +2,23 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello world!")
-
+func constants() {
 	// Constant
 	const PI float64 = 3.1415
 
 	fmt.Println("Hello world!")
+}
 
+func declarations() {
 	// Declaration
 	base := 12
 	var height int = 14
 	var area int
 
 	fmt.Println(base, height, area)
+}
 
+func zerovalues() {
 	// Zero values
 	var a int
 	var b float64
@@ -24,10 +26,39 @@ func main() {
 	var d bool
 
 	fmt.Println(a, b, c, d)
+}
 
+func areaSqr(base int) {
 	// Area
-	const baseSquare = 10
-	const areaSquare = baseSquare * baseSquare
+	area := base * base
 
-	fmt.Println("Area:", areaSquare)
+	fmt.Println("Area:", area)
+}
+
+func sum(x int, y int) {
+	// Sum
+	result := x + y
+	fmt.Println("Sum:", result)
+}
+
+func doubleReturn(a int) (int, int) {
+	return a, 2 * a
+}
+
+func main() {
+	fmt.Println("Hello world!")
+
+	constants()
+
+	declarations()
+
+	zerovalues()
+
+	const baseSquare = 10
+	areaSqr(baseSquare)
+
+	sum(10, 50)
+
+	val1, val2 := doubleReturn(2)
+	fmt.Println("val1:", val1, ",", "val2:", val2)
 }
