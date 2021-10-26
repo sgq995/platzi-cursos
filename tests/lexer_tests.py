421c87889f7a612ea30255f4a51741c5e79bdd55
Sebastian G. Q <sgq995@gmail.com>
2021-10-26 11:41:25 -0500

    Clase 6

--- tests/lexer_tests.py
+++ tests/lexer_tests.py
@@ -25,3 +25,52 @@ class LexerTest(TestCase):
     ]
 
     self.assertEquals(tokens, expected_tokens)
+
+  def test_one_character_operator(self) -> None:
+    source: str = '=+'
+    lexer: Lexer = Lexer(source)
+
+    tokens: List[Token] = []
+    for i in range(len(source)):
+      tokens.append(lexer.next_token())
+    
+    expected_tokens: List[Token] = [
+      Token(TokenType.ASSIGN, '='),
+      Token(TokenType.PLUS, '+'),
+    ]
+
+    self.assertEquals(tokens, expected_tokens)
+
+  def test_eof(self) -> None:
+    source: str = '+'
+    lexer: Lexer = Lexer(source)
+
+    tokens: List[Token] = []
+    for i in range(len(source) + 1):
+      tokens.append(lexer.next_token())
+
+    expected_tokens: List[Token] = [
+      Token(TokenType.PLUS, '+'),
+      Token(TokenType.EOF, ''),
+    ]
+
+    self.assertEquals(tokens, expected_tokens)
+
+  def test_delimiters(self) -> None:
+    source: str = '(){},;'
+    lexer: Lexer = Lexer(source)
+
+    tokens: List[Token] = []
+    for i in range(len(source)):
+      tokens.append(lexer.next_token())
+    
+    expected_tokens: List[Token] = [
+      Token(TokenType.LPAREN, '('),
+      Token(TokenType.RPAREN, ')'),
+      Token(TokenType.LBRACE, '{'),
+      Token(TokenType.RBRACE, '}'),
+      Token(TokenType.COMMA, ','),
+      Token(TokenType.SEMICOLON, ';'),
+    ]
+
+    self.assertEquals(tokens, expected_tokens)
