a54dec168cdcde07c78302b2f38a467baa4e0070
Sebastian G. Q <sgq995@gmail.com>
2021-11-22 12:33:57 -0500

    Styles

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -11,6 +11,7 @@ import { Product } from '../product.model';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
   @Input() product!: Product;
