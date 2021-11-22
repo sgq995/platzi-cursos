863bf9508f7b640e7594ad18c8fa48d062256f16
Sebastian G. Q <sgq995@gmail.com>
2021-11-22 12:26:33 -0500

    Lifecycle

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  SimpleChanges,
+} from '@angular/core';
 
 import { Product } from '../product.model';
 
@@ -9,6 +15,26 @@ import { Product } from '../product.model';
 export class ProductComponent {
   @Input() product!: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    console.log('1. constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('2. ngOnChanges', changes);
+  }
+
+  ngOnInit() {
+    console.log('3. ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('4. ngDoCheck');
+  }
+
+  ngOnDestroy() {
+    console.log('5. ngOnDestroy');
+  }
 
   addCart() {
     console.log('Agregar al carrito');
