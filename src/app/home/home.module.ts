a872b06ce64e23ef46c44ecb16007d25c56be059
Sebastian G. Q <sgq995@gmail.com>
2021-11-24 11:21:42 -0500

    Core & Shared Modules

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,7 +7,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +16,7 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     HomeRoutingModule,
     SwiperModule,
   ],
