752cb7403df58c279ece9e37b10f32e75624dc8a
Sebastian G. Q <sgq995@gmail.com>
2021-11-26 17:10:40 -0500

    Angular Material Schematics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,6 +39,13 @@ const routes: Routes = [
     ],
   },
   {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then(
+        (m) => m.AdminModule
+      ),
+  },
+  {
     path: '**',
     loadChildren: () =>
       import('./page-not-found/page-not-found.module').then(
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+
 
 @NgModule({
   declarations: [],
@@ -16,6 +20,9 @@ import { MatCardModule } from '@angular/material/card';
     MatIconModule,
     MatBadgeModule,
     MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
   ],
   exports: [
     MatButtonModule,
@@ -23,6 +30,9 @@ import { MatCardModule } from '@angular/material/card';
     MatIconModule,
     MatBadgeModule,
     MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
   ],
 })
 export class MaterialModule {}
