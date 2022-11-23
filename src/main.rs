4bb3b1c63c1ada7f2793f9c3e9a9f007d8a0b305
Sebastian G. Q <sgq995@gmail.com>
2022-11-22 21:01:05 -0500

    Structs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,27 @@ use csv::{ReaderBuilder, StringRecord};
 
 const FILENAME: &str = "history.csv";
 
+#[derive(Debug)]
+struct HistoryLine {
+    data_type: String,
+    tag: String,
+    text: String,
+    life: i32,
+}
+
+impl HistoryLine {
+    fn new(row: &StringRecord) -> HistoryLine {
+        return HistoryLine {
+            data_type: row.get(0).unwrap().to_string(),
+            tag: row.get(1).unwrap().to_string(),
+            text: row.get(2).unwrap().to_string(),
+            life: row.get(3).unwrap().trim().parse().unwrap_or(0),
+        };
+    }
+}
+
 fn main() {
+    let mut history: Vec<HistoryLine> = vec![];
     let content = fs::read_to_string(FILENAME).unwrap();
 
     let mut rdr = ReaderBuilder::new()
@@ -12,6 +32,11 @@ fn main() {
         .from_reader(content.as_bytes());
 
     for result in rdr.records() {
-        println!("{:?}", result.unwrap().get(2).unwrap().trim());
+        let line = result.unwrap();
+        let history_line = HistoryLine::new(&line);
+
+        history.push(history_line);
+
+        println!("{:?}", line);
     }
 }
