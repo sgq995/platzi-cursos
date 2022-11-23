d4d373afcd3271fab185a26d718c2a7a3ce641f4
Sebastian G. Q <sgq995@gmail.com>
2022-11-22 19:31:09 -0500

    Read stdin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
-    
+    println!("Introduce yourself: ");
+    let mut name: String = String::new();
+    std::io::stdin().read_line(&mut name).unwrap();
+    name = name.trim().to_string();
 
-    println!("Hello, world!");
+    println!("How old are you? ");
+    let mut age_str: String = String::new();
+    std::io::stdin().read_line(&mut age_str).unwrap();
+    let age: i8 = age_str.trim().parse().unwrap();
+
+    println!("Where are you from? ");
+    let mut country: String = String::new();
+    std::io::stdin().read_line(&mut country).unwrap();
+    country = country.trim().to_string();
+
+    println!("Welcome {} from {}, your age is {}", name, country, age);
 }
