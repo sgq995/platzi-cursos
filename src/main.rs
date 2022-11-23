bbdbc0e09b69d5bd512a4c84fe573203b389e419
Sebastian G. Q <sgq995@gmail.com>
2022-11-22 19:35:14 -0500

    Conditionals

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 fn main() {
-    println!("Introduce yourself: ");
-    let mut name: String = String::new();
-    std::io::stdin().read_line(&mut name).unwrap();
-    name = name.trim().to_string();
-
     println!("How old are you? ");
     let mut age_str: String = String::new();
     std::io::stdin().read_line(&mut age_str).unwrap();
     let age: i8 = age_str.trim().parse().unwrap();
 
-    println!("Where are you from? ");
-    let mut country: String = String::new();
-    std::io::stdin().read_line(&mut country).unwrap();
-    country = country.trim().to_string();
-
-    println!("Welcome {} from {}, your age is {}", name, country, age);
+    if age < 18 {
+        println!("You should wait at least {} years", 18 - age);
+    } else {
+        println!("You can enter");
+    }
 }
