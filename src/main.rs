2a6d42694e659c985f29da373cbe5486a001dc2f
Sebastian G. Q <sgq995@gmail.com>
2022-11-22 20:22:38 -0500

    Calculator

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
+use regex::Regex;
+
 fn main() {
-    println!("Let's play 21");
+    println!("Calculator");
 
-    let mut sum: i8 = 0;
+    // Regex
+    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
+    let re_mult= Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
 
+    // User
+    println!("Introduce your expression: ");
+    let mut expression = String::new();
+    std::io::stdin().read_line(&mut expression).unwrap();
+
+    // Mult
     loop {
-        println!("Which card do you have? ");
-        let mut card: String = String::new();
-        std::io::stdin().read_line(&mut card).unwrap();
-
-        let number: i8 = card.trim().parse().unwrap();
-        sum += number;
-
-        if sum >= 21 {
+        let caps = re_mult.captures(expression.as_str()); if caps.is_none() {
             break;
         }
+        let caps = caps.unwrap();
+
+        let caps_expression = caps.get(0).unwrap().as_str();
+
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        println!("{:?}, left: {}, right: {}", caps, left_value, right_value);
+
+        let result = left_value * right_value;
+
+        expression = expression.replace(caps_expression, &result.to_string());
     }
 
-    println!("Your result: {}", sum);
+    // Sum
+    loop {
+        let caps = re_add.captures(expression.as_str());
+        if caps.is_none() {
+            break;
+        }
+        let caps = caps.unwrap();
+
+        let caps_expression = caps.get(0).unwrap().as_str();
+
+        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+        println!("{:?}, left: {}, right: {}", caps, left_value, right_value);
+
+        let result = left_value + right_value;
+
+        expression = expression.replace(caps_expression, &result.to_string());
+    }
+
+    // Result
+    println!("Result: {}", expression);
 }
