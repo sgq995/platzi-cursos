b5d7a154ef4ec9c644ae4e02fe1bc961ccd19e17
Sebastian G. Q <sgq995@gmail.com>
2022-11-22 20:28:28 -0500

    Arrays & for

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,14 @@
-use regex::Regex;
-
 fn main() {
-    println!("Calculator");
+    println!("Arrays");
 
-    // Regex
-    let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
-    let re_mult= Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
+    let mut names = Vec::new();
 
-    // User
-    println!("Introduce your expression: ");
-    let mut expression = String::new();
-    std::io::stdin().read_line(&mut expression).unwrap();
-
-    // Mult
-    loop {
-        let caps = re_mult.captures(expression.as_str()); if caps.is_none() {
-            break;
-        }
-        let caps = caps.unwrap();
-
-        let caps_expression = caps.get(0).unwrap().as_str();
-
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        println!("{:?}, left: {}, right: {}", caps, left_value, right_value);
-
-        let result = left_value * right_value;
-
-        expression = expression.replace(caps_expression, &result.to_string());
+    for _i in 0..3 {
+        println!("Type a name:");
+        let mut name = String::new();
+        std::io::stdin().read_line(&mut name).unwrap();
+        names.push(name.trim().to_string());
     }
 
-    // Sum
-    loop {
-        let caps = re_add.captures(expression.as_str());
-        if caps.is_none() {
-            break;
-        }
-        let caps = caps.unwrap();
-
-        let caps_expression = caps.get(0).unwrap().as_str();
-
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-        println!("{:?}, left: {}, right: {}", caps, left_value, right_value);
-
-        let result = left_value + right_value;
-
-        expression = expression.replace(caps_expression, &result.to_string());
-    }
-
-    // Result
-    println!("Result: {}", expression);
+    println!("{:?}", names);
 }
