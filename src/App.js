6a58a8b1872a7b9797ab4ac39fbd27e76db76835
Sebastian G. Q <sgq995@gmail.com>
2022-11-23 20:44:38 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Badges from './pages/Badges';
+import BadgeNew from './pages/BadgeNew';
+import BadgeEdit from './pages/BadgeEdit';
+import BadgeDetails from './pages/BadgeDetailsContainer';
+import Layout from './components/Layout';
+import RickAndMorty from './pages/RickAndMorty';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/badges" component={Badges} />
+          <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
+          <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+          <Route exact path="/rick_and_morty" component={RickAndMorty} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
