05fd27c1a631b952ccf2c99b92efa6a69fa2a4ae
Sebastian G. Q <sgq995@gmail.com>
2022-11-27 16:04:42 -0500

    Insert post

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -6,16 +6,31 @@ use std::env;
 use diesel::{Connection, RunQueryDsl, SqliteConnection};
 use dotenvy::dotenv;
 
-use crate::{models::Post, schema::posts};
+use crate::{models::NewPost, models::Post, schema::posts};
+
+fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL was not found");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+fn create_post(conn: &mut SqliteConnection, title: &str, body: &str, slug: &str) -> usize {
+    let new_post = NewPost { title, body, slug };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .execute(conn)
+        .expect("Failed to insert")
+}
 
 fn main() {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL was not found");
-
-    let mut conn = SqliteConnection::establish(&database_url).expect("Couldn't connect to sqlite");
+    let conn = &mut establish_connection();
+    create_post(conn, "My second post", "Lorem ipsum", "first-post");
 
     let posts_result = posts::dsl::posts
-        .load::<Post>(&mut conn)
+        .load::<Post>(conn)
         .expect("Something goes wrong");
 
     for post in posts_result {
--- rust-backend/src/models.rs
+++ rust-backend/src/models.rs
@@ -1,5 +1,7 @@
 use diesel::prelude::*;
 
+use crate::schema::posts;
+
 #[derive(Queryable)]
 pub struct Post {
     pub id: i32,
@@ -7,4 +9,12 @@ pub struct Post {
     pub slug: String,
     pub body: String,
     pub published: bool,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = posts)]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+    pub slug: &'a str,
 }
