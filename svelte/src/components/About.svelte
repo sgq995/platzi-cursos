3bf73061e27b452d0275f4910f3cc9757659d4ff
Sebastian G. Q <sgq995@gmail.com>
2023-01-24 20:35:37 -0500

    Svelte

---
+++ svelte/src/components/About.svelte
@@ -0,0 +1,34 @@
+<script>
+  let text = 'Frontend Svelte';
+  let count = 0;
+  let styles = { darkMode: false };
+
+  function handleClick() {
+    count += 1;
+  }
+
+  function toggle() {
+    styles.darkMode = !styles.darkMode;
+    document.body.classList.toggle('dark-mode');
+  }
+</script>
+
+{#if !styles.darkMode}
+  <p>{text}</p>
+{:else}
+  <p><span>Hello dark mode</span></p>
+{/if}
+
+<p>
+  {text}
+</p>
+
+<button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
+
+<button on:click={toggle}>DarkMode</button>
+
+<style>
+  p {
+    color: var(--theme-color);
+  }
+</style>
---
+++ svelte/src/components/Person.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name;
+  export let lastName;
+  export let age;
+</script>
+
+<p>
+  <span>{name}</span>
+  <span>{lastName}</span>
+  <span>{age}</span>
+</p>
