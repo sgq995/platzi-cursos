300f4accc808852823d1cf102c25ea68018d1648
Sebastian G. Q <sgq995@gmail.com>
2023-07-11 18:49:15 -0500

    java: messages

---
+++ java-se-persistencia/src/Message.java
@@ -0,0 +1,14 @@
+public class Messages {
+    int id;
+
+    String message;
+
+    String author;
+
+    String timestamp;
+
+    public Messages( String message, String author, String timestamp ) {
+
+    }
+
+}
