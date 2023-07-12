0dfb03a5288b926d644b15f4ed068f2688f7bf40
Sebastian G. Q <sgq995@gmail.com>
2023-07-11 20:12:17 -0500

    java: project structure

--- java-spring/src/main/java/com/segoqu/market/web/controller/HelloWorldController.java
+++ java-spring/src/main/java/com/segoqu/market/web/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.segoqu.market;
+package com.segoqu.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
